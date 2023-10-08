ae982d633d4bac66757667024ae299d9fed1b384
MD Razikul Islam Joni <razikul.joni69@gmail.com>
2023-10-08 17:19:15 +0600

    Problem-1 solved

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,53 +1,111 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Problem1 = () => {
+    const [show, setShow] = useState("all");
+    const [data, setData] = useState([]);
 
-    const [show, setShow] = useState('all');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const name = formData.get("name");
+        const status = formData.get("status");
+        setData([...data, { name, status }]);
+    };
 
-    const handleClick = (val) =>{
+    const handleClick = (val) => {
         setShow(val);
-    }
+    };
+
+    const filteredData =
+        show === "all" ? data : data.filter((item) => item.status === show);
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+                <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="row gy-2 gx-3 align-items-center mb-4"
+                    >
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                                name="name"
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                            />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input
+                                name="status"
+                                type="text"
+                                className="form-control"
+                                placeholder="Status"
+                            />
                         </div>
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
                         </div>
                     </form>
                 </div>
                 <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <ul
+                        className="nav nav-pills mb-3"
+                        id="pills-tab"
+                        role="tablist"
+                    >
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button
+                                className={`nav-link ${
+                                    show === "all" && "active"
+                                }`}
+                                type="button"
+                                onClick={() => handleClick("all")}
+                            >
+                                All
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button
+                                className={`nav-link ${
+                                    show === "active" && "active"
+                                }`}
+                                type="button"
+                                onClick={() => handleClick("active")}
+                            >
+                                Active
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button
+                                className={`nav-link ${
+                                    show === "completed" && "active"
+                                }`}
+                                type="button"
+                                onClick={() => handleClick("completed")}
+                            >
+                                Completed
+                            </button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
-                    <table className="table table-striped ">
+                    <table className="table table-striped">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {filteredData.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>{item.status}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
